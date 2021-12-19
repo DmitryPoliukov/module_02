71e0ff1479154ecf773d113de507c0fe488e043e
DmitryPoliukov <lilomoshad@gmail.com>
2021-12-20 02:30:45 +0300

    small changes

--- api/src/main/java/com/epam/esm/controller/advice/Advice.java
+++ api/src/main/java/com/epam/esm/controller/advice/Advice.java
@@ -8,25 +8,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public class Advice {
 
     @ExceptionHandler(value = {ResourceNotFoundException.class})
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e) {
-        String errorCode = String.format("%s%s", BAD_REQUEST.value(), e.getResourceId());
+        String errorCode = String.format("%s%s", NOT_FOUND.value(), e.getResourceId());
         ErrorResponse errorResponse =
                 new ErrorResponse(e.getMessage(), errorCode);
-        return new ResponseEntity<>(errorResponse, BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse, NOT_FOUND);
     }
 
     @ExceptionHandler(value = {ResourceValidationException.class})
     public ResponseEntity<ErrorResponse> handleResourceValidationException(ResourceValidationException e) {
-        String errorCode = String.format("%s%s", BAD_REQUEST.value(), e.getResourceId());
+        String errorCode = String.format("%s%s", INTERNAL_SERVER_ERROR.value(), e.getResourceId());
         ErrorResponse errorResponse =
                 new ErrorResponse(e.getMessage(), errorCode);
-        return new ResponseEntity<>(errorResponse, BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse, INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<ErrorResponse> handleAllException(Exception e) {
+        String errorCode = String.format("%s%s", INTERNAL_SERVER_ERROR.value(), 00);
+        ErrorResponse errorResponse =
+                new ErrorResponse(e.getMessage(), errorCode);
+        return new ResponseEntity<>(errorResponse, INTERNAL_SERVER_ERROR);
     }
 
 
--- api/src/main/java/com/epam/esm/controller/conf/AppInitializer.java
+++ api/src/main/java/com/epam/esm/controller/conf/AppInitializer.java
@@ -1,5 +1,10 @@
 package com.epam.esm.controller.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -7,10 +12,13 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import java.sql.ResultSet;
+import java.util.Map;
 
 public class AppInitializer implements WebApplicationInitializer {
+
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    public void onStartup(ServletContext servletContext) {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
         context.register(SpringConfig.class);
         context.setServletContext(servletContext);
@@ -19,4 +27,6 @@ public class AppInitializer implements WebApplicationInitializer {
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
     }
+
+
 }
