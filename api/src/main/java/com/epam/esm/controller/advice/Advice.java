992b847d255905bb2f9ad02914ad156892e92f56
DmitryPoliukov <lilomoshad@gmail.com>
2021-12-20 11:23:47 +0300

    refactor dao with adbsractCRDrepo

--- api/src/main/java/com/epam/esm/controller/advice/Advice.java
+++ api/src/main/java/com/epam/esm/controller/advice/Advice.java
@@ -4,15 +4,20 @@ package com.epam.esm.controller.advice;
 import com.epam.esm.repository.entity.ErrorResponse;
 import com.epam.esm.service.exception.ResourceNotFoundException;
 import com.epam.esm.service.exception.ResourceValidationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
-public class Advice {
+public class Advice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ResourceNotFoundException.class})
     public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e) {
@@ -24,10 +29,10 @@ public class Advice {
 
     @ExceptionHandler(value = {ResourceValidationException.class})
     public ResponseEntity<ErrorResponse> handleResourceValidationException(ResourceValidationException e) {
-        String errorCode = String.format("%s%s", INTERNAL_SERVER_ERROR.value(), e.getResourceId());
+        String errorCode = String.format("%s%s", BAD_REQUEST.value(), e.getResourceId());
         ErrorResponse errorResponse =
                 new ErrorResponse(e.getMessage(), errorCode);
-        return new ResponseEntity<>(errorResponse, INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorResponse, BAD_REQUEST);
     }
 
     @ExceptionHandler(value = {Exception.class})
@@ -38,5 +43,13 @@ public class Advice {
         return new ResponseEntity<>(errorResponse, INTERNAL_SERVER_ERROR);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleHttpRequestMethodNotSupported(ex, headers, status, request);
+    }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleHttpMediaTypeNotSupported(ex, headers, status, request);
+    }
 }
